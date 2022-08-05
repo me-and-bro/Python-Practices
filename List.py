352c4cf44bcd031ceb3308246346bca61562eebf
Dojin Park <djpark1674@gmail.com>
2022-08-05 18:47:55 +0900

    first commit test

---
+++ List.py
@@ -0,0 +1,58 @@
+## List
+#
+# subway = [10, 20, 30]
+# print(subway)
+#
+## Index
+#
+# subway = ["사과", "배", "포도"]
+# print(subway)
+#
+# print(subway.index("사과"))
+#
+## Append
+#
+# subway.append("복숭아")
+# print(subway)
+#
+## Insert
+#
+# subway.insert(1, "수박")
+# print(subway)
+#
+## Pop
+#
+# print(subway.pop())
+# print(subway)
+#
+# print(subway.pop())
+# print(subway)
+#
+# print(subway.pop())
+# print(subway)
+#
+## Count
+#
+# subway.append("수박")
+# print(subway.count("수박"))
+
+# # Sort
+#
+# num_list = [6,3,4,1,2]
+# num_list.sort()
+# print(num_list)
+#
+# # Reverse
+#
+# num_list.reverse()
+# print(num_list)
+#
+# #Clear
+#
+# num_list.clear()
+# print(num_list)
+#
+# #Extend
+#
+# num_list.extend(num_list)
+# print(num_list)
